7bba5dc6d1493682993acb47bc473affe466b4ee
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-03-23 20:09:16 +0200

    feat: add Currency model

---
+++ CurrencyObserver/CurrencyObserver.WebAPI/Models/Currency.cs
@@ -0,0 +1,8 @@
+﻿namespace CurrencyObserver.WebAPI.Models
+{
+    public class Currency
+    {
+        public required string Name { get; set; }
+        public required decimal USDPrice { get; set; }
+    }
+}
