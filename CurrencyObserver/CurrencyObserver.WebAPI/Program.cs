58953a85bae7b39bb7d1d13747d6d69bbcd95e0a
VolodymyrShapoval <volodymyr.sh.05@gmail.com>
2025-03-27 00:59:56 +0200

    feat: add swagger, set up swagger in Program.cs, add test map

--- CurrencyObserver/CurrencyObserver.WebAPI/Program.cs
+++ CurrencyObserver/CurrencyObserver.WebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CurrencyObserver API", Description = "Presenting all currencies you want!", Version = "v1" });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "CurrencyObserver API V1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -19,5 +34,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Test maps
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
