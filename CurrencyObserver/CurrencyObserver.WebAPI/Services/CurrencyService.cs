6c810bbf2517f5bfe231a73976b907d8705dc0f7
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-03-27 15:28:08 +0200

    feat: implement CurrencyService

---
+++ CurrencyObserver/CurrencyObserver.WebAPI/Services/CurrencyService.cs
@@ -0,0 +1,21 @@
+﻿using CurrencyObserver.WebAPI.Interfaces;
+using CurrencyObserver.WebAPI.Models;
+
+namespace CurrencyObserver.WebAPI.Services
+{
+    public class CurrencyService : ICurrencyService
+    {
+        private readonly ICurrencyParser _parser;
+        private readonly HttpClient _httpClient;
+        public CurrencyService(ICurrencyParser parser, HttpClient httpClient) 
+        {
+            _parser = parser;
+            _httpClient = httpClient;
+        }
+        public async Task<Currency?> GetCurrencyAsync(string abbreviation)
+        {
+            var response = await _httpClient.GetAsync($"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={abbreviation}&json");
+            return await _parser.ParseCurrencyAsync(response.Content.ToString());
+        }
+    }
+}
