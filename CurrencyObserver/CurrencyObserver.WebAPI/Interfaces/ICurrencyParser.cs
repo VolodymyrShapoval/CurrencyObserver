c5e8f3c8338b03b42e769ebe3d3b2c192e056cc3
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-03-27 15:25:30 +0200

    feat: add ICurrencyParser interface

---
+++ CurrencyObserver/CurrencyObserver.WebAPI/Interfaces/ICurrencyParser.cs
@@ -0,0 +1,9 @@
+﻿using CurrencyObserver.WebAPI.Models;
+
+namespace CurrencyObserver.WebAPI.Interfaces
+{
+    public interface ICurrencyParser
+    {
+        Task<Currency?> ParseCurrencyAsync(string? jsonResponse);
+    }
+}
