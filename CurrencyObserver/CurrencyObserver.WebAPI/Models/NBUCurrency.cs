2fc01ea6d48d70050bb3e16daf43cfd2cd9beb92
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-03-27 16:40:00 +0200

    feat: add NBUCurrency model

---
+++ CurrencyObserver/CurrencyObserver.WebAPI/Models/NBUCurrency.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CurrencyObserver.WebAPI.Models
+{
+    public class NBUCurrency
+    {
+        [JsonPropertyName("r030")]
+        public int R030 { get; set; }
+
+        [JsonPropertyName("txt")]
+        public string Title { get; set; } = string.Empty;
+
+        [JsonPropertyName("rate")]
+        public decimal UAHRate { get; set; }
+
+        [JsonPropertyName("cc")]
+        public string Abbreviation { get; set; } = string.Empty;
+
+        [JsonPropertyName("exchangedate")]
+        public string ExchangeDate { get; set; } = string.Empty;
+    }
+}
