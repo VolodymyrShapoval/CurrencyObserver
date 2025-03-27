d41e903fa82249fb35a8c1800f96ab1e04cc8723
VolodymyrS <volodymyr.sh.05@gmail.com>
2025-03-27 16:40:50 +0200

    feat: add NBUCurrencyParser, implement ICurrencyParser interface

---
+++ CurrencyObserver/CurrencyObserver.WebAPI/Services/NBUCurrencyParser.cs
@@ -0,0 +1,43 @@
+﻿using CurrencyObserver.WebAPI.Interfaces;
+using CurrencyObserver.WebAPI.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using System.Text.Json;
+
+namespace CurrencyObserver.WebAPI.Services
+{
+    public class NBUCurrencyParser : ICurrencyParser
+    {
+        private readonly ILogger<NBUCurrencyParser> _logger;
+        public NBUCurrencyParser(ILogger<NBUCurrencyParser> logger)
+        {
+            _logger = logger;
+        }
+        public Currency? ParseCurrency(string? jsonResponse)
+        {
+            if (String.IsNullOrEmpty(jsonResponse))
+            {
+                return null;
+            }
+
+            try
+            {
+                var currencies = JsonSerializer.Deserialize<List<NBUCurrency>>(jsonResponse);
+                var NBUcurrency = currencies?.FirstOrDefault();
+                if (NBUcurrency == null)
+                {
+                    return null;
+                }
+                return new Currency
+                {
+                    Abbreviation = NBUcurrency.Abbreviation,
+                    Rate = 1 / NBUcurrency.UAHRate
+                };
+            }
+            catch (JsonException ex)
+            {
+                _logger.LogError(ex, "Error parsing JSON");
+                return null;
+            }
+        }
+    }
+}
