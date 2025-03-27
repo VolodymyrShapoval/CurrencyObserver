50eea597ddd7846fc8252aadf0ff224b0350128f
Vektor19 <viktor.komissarov@knu.ua>
2025-03-27 22:24:08 +0200

    style: update ProfileImage component, change heading styles in Title component

--- CurrencyObserver.Client/src/components/Main/Introduction/ProfileImage.js
+++ CurrencyObserver.Client/src/components/Main/Introduction/ProfileImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ProfileImage = () => (
-  <img className="introduction-image" src="/assets/images/tom_cruise_photo.png" alt="Profile" />
+  <img className="introduction-image" src="/assets/images/observerImg.webp" alt="Profile" />
 );
 
 export default ProfileImage;
--- CurrencyObserver.Client/src/components/Main/Introduction/Title.js
+++ CurrencyObserver.Client/src/components/Main/Introduction/Title.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Title = () => (
   <div>
-    <h1 id="name-title">Volodymyr Shapoval</h1>
-    <h1 id="developer-title">Full-stack developer</h1>
+    <h1 id="name-title" style={{fontWeight:"700"}}>Currency Observer</h1>
+    <h2 id="description-title" style={{fontSize: "0.6em", marginTop:"1em"}} >Get real-time currency updates and stay one step ahead of financial changes!</h2>
   </div>
 );
 
