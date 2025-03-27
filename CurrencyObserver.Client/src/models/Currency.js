54a73d035cd7da15a12c23b0dc5dd2cdcbf4e33b
Vektor19 <viktor.komissarov@knu.ua>
2025-03-27 22:57:11 +0200

    feat: create Currency model, delete unused models

---
+++ CurrencyObserver.Client/src/models/Currency.js
@@ -0,0 +1,10 @@
+export class Currency {
+    constructor(abbreviation, rate) {
+        this.abbreviation = abbreviation;
+        this.rate = rate;
+    }
+
+    getValues() {
+        return this.abbreviation, this.rate;
+    }
+}
