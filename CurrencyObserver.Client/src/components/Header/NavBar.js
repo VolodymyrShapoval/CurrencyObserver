4f0f483fe4e261736f9d5ad206c9c95ab7a5da2a
Vektor19 <viktor.komissarov@knu.ua>
2025-03-27 22:29:37 +0200

    style: clean-up of code, delete Projects, About components

--- CurrencyObserver.Client/src/components/Header/NavBar.js
+++ CurrencyObserver.Client/src/components/Header/NavBar.js
@@ -4,8 +4,6 @@ const NavBar = () => (
   <nav className="menu">
     <ul>
       <li><a href="#main-page">Home</a></li>
-      <li><a href="#about-me-page">About</a></li>
-      <li><a href="#my-projects-page">Portfolio</a></li>
       <li><a href="#my-blog-page">Blog</a></li>
       <li><a href="#contact-me-page">Contact</a></li>
     </ul>
--- CurrencyObserver.Client/src/components/Main/MainPage.js
+++ CurrencyObserver.Client/src/components/Main/MainPage.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import Introduction from './Introduction/Introduction';
-import AboutMe from './AboutMe/AboutMe';
-import Projects from './Projects/Projects';
 import Blog from './Blog/Blog';
 import Contact from './Contact/Contact';
 
 const MainPage = () => (
   <main className="main-page" id="main-page">
     <Introduction />
-    <AboutMe />
-    <Projects />
     <Blog />
     <Contact />
   </main>
