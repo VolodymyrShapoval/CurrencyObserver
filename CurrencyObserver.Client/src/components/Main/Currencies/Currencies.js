f7d814d6842e0cebaf383916007d1f5e81c34dcb
Vektor19 <viktor.komissarov@knu.ua>
2025-03-28 14:54:43 +0200

    feat: implement currency selection and display in Currencies component

--- CurrencyObserver.Client/src/components/Main/Currencies/Currencies.js
+++ CurrencyObserver.Client/src/components/Main/Currencies/Currencies.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import CurrencyService from "../../../services/CurrencyService";
+import CurrencyComponent from "./CurrencyComponent";
 
 const Currencies = () => {
+  const [baseCurrency, setBaseCurrency] = useState(null);
   const [currencies, setCurrencies] = useState([]);
 
   useEffect(() => {
@@ -13,8 +15,18 @@ const Currencies = () => {
         }
         const data = await response.json();
         setCurrencies(data); 
+        setBaseCurrency(data[1]);
       } catch (error) {
         console.error("Error fetching currencies:", error);
+        //test data
+        const data = [
+          { abbreviation: "USD", rate: 1 },
+          { abbreviation: "EUR", rate: 0.85 },
+          { abbreviation: "GBP", rate: 0.75 },
+          { abbreviation: "UAH", rate: 41.5 },
+        ];
+        setCurrencies(data); 
+        setBaseCurrency(data[3]);
       }
     };
 
@@ -23,6 +35,40 @@ const Currencies = () => {
 
   return (
     <section className="my-currencies-page" id="my-currencies-page">
+      <h2 className="section-title"><strong>Currencies</strong></h2>
+      <div className="base-currency-header">
+        <h2>Base currency: </h2>
+        <select 
+          onChange={(e) => setBaseCurrency(currencies.find(currency => currency.abbreviation === e.target.value))} 
+          value={baseCurrency?.abbreviation}
+          style={{
+            padding: "8px",
+            fontSize: "16px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+            outline: "none",
+            cursor: "pointer",
+          }}
+        >
+          {currencies.map((currency) => (
+            <option key={currency.abbreviation} value={currency.abbreviation}>
+              {currency.abbreviation}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="currencies-header">
+        <p>Currency</p>
+        <p>Rate</p>
+        <p>Price per one</p>
+      </div>
+      <div className="currencies-container">
+          {currencies
+            .filter(currency => currency.abbreviation !== baseCurrency?.abbreviation)
+            .map((currency) => (
+              <CurrencyComponent key={currency.abbreviation} currency={currency} baseCurrency={baseCurrency}/>
+          ))}
+      </div>
     </section>
   );
 };
---
+++ CurrencyObserver.Client/src/components/Main/Currencies/CurrencyComponent.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import CurrencyService from "../../../services/CurrencyService";
+
+const CurrencyComponent = ({currency, baseCurrency}) => {
+    const [currencyConverted, setCurrencyConverted] = useState(null);
+    useEffect(() => {
+        setCurrencyConverted(CurrencyService.convertCurrency(baseCurrency, currency, 1));
+      }, [baseCurrency]);
+        
+  return (
+    <div className="currency-item">
+        <p>{currency.abbreviation}</p>
+        <p>{Math.round(currencyConverted * 1000) / 1000}</p>
+        <p>{Math.round((1 / currencyConverted) * 1000) / 1000}</p>
+    </div>
+  );
+};
+
+export default CurrencyComponent;
